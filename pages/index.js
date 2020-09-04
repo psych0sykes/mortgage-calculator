da9a8b6fca2acdd0dd6260772fcc9be8692e4b75
psych0sykes <blakesykes13@gmail.com>
2020-09-04 00:24:44 -0600

    Clean out some unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,8 @@
 import Section from "../components/Section";
-import Input from "../components/Form/Input";
-import {useState} from "react";
-import useFormatNumber from "../hooks/useFormatNumber";
 import Payment from "../components/Tabs/Payment";
-import Affordable from "../components/Tabs/Affordabie";
 import NavTabs from "../components/NavTabs";
 
 export default function Home() {
-
-  const [activeTab,setActiveTab] = useState("payment")
-
-  const Tabs = (props) => {
-    switch(props.tab){
-      case "payment":
-        return <Payment/>;
-      case "affordable":
-        return <Affordable/>;
-    }
-  };
-
-  const handleNavTab = (tab) => {
-    setActiveTab(tab);
-  };
 
   return (
     <div>
@@ -31,7 +12,7 @@ export default function Home() {
           </h1>
           <NavTabs/>
           <Section>
-            <Tabs tab={activeTab}/>
+            <Payment/>
           </Section>
       </Section>
     </div>
